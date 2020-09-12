537b1331815ff9ba319bd655ee6b2d364df5b342
TheWolfSpartan <eduardojgila@gmail.com>
2020-09-12 15:49:58 -0500

    Initial comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hola-mundo';
+  title = 'Grupo de Séptimo';
+  frutas = ['Manzana','fresa','Naranja','Uva'];
 }
